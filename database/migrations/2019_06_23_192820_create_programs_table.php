55675bb86553265938cf3e48e2244e5f7485facd
nekosama <kevinorge@gmail.com>
2019-06-24 03:48:03 +0800

    init migration

---
+++ database/migrations/2019_06_23_192820_create_programs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProgramsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('programs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('created_by');
+            $table->dateTime('created_at');
+            $table->string('modified_by');
+            $table->dateTime('modified_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('programs');
+    }
+}
