e043c91c66176cb9128d2b86731d94273bd7621c
nekosama <kevinorge@gmail.com>
2019-06-24 05:34:26 +0800

    fix jquery link

--- resources/views/bottom.blade.php
+++ resources/views/bottom.blade.php
@@ -1,4 +1,4 @@
 	</body>
-	<script src="libs/jquery/jquery-3.4.1.js"></script>
+	<script src="/libs/jquery/jquery-3.4.1.js"></script>
 	<script src="/libs/bootstrap/js/bootstrap.min.js"></script>
 </html>
