7686ff0b8a63f713a46063d62ef9fd9bae30965c
nekosama <kevinorge@gmail.com>
2019-09-15 03:00:46 +0800

    template structure

---
+++ resources/views/layouts/admin/navbar.blade.php
@@ -0,0 +1,13 @@
+<div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
+	<nav class="uk-navbar-container" uk-navbar>
+		<div class="uk-navbar-left">
+					<ul class="uk-navbar-nav">
+							<li class="uk-active"><a href="">Home</a></li>
+							<li><a href="/admin/teachers">Teachers</a></a></li>
+							<li><a href="/admin/students">Students</a></li>
+							<li><a href="/admin/classrooms">Classrooms</a></li>
+							<li><a href="/admin/subjects">Subjects</a></li>
+					</ul>
+			</div>
+	</nav>
+</div>
