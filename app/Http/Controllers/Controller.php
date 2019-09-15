d5a577ef7fde4c8a044726291de78df5f1a43d97
nekosama <kevinorge@gmail.com>
2019-09-16 03:55:18 +0800

    tpl retrieval

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,19 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Config;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+        Config::set('template_dir', '');
+    }
+
+    public function getTpl($name)
+    {
+        return Config::get('template_dir').$name;
+    }
 }
