e93409cb9ef3a5a3dff398619fc004c4aba02da6
nekosama <kevinorge@gmail.com>
2019-09-26 09:22:45 +0800

    extend main in teachers template

--- resources/views/teachers/index.blade.php
+++ resources/views/teachers/index.blade.php
@@ -1,5 +1,13 @@
+@extends("layouts.main")
+
+@section("title")
+Teachers List
+@endsection
+
+@section("content")
 <ul>
 @foreach($items as $item)
 	<li>{{ $item->id }} : {{ $item->firstname }} {{ $item->lastname }}</li>
 @endforeach
 </ul>
+@endsection
