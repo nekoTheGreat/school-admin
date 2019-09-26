522ac602ee59ecaee7adc6ff612668d5ae0e844e
nekosama <kevinorge@gmail.com>
2019-09-26 09:00:10 +0800

    sample join and paging

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -6,12 +6,25 @@ use Illuminate\Http\Request;
 
 use App\Models\Teacher;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 
 class TeacherController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $records = Teacher::all();
+        $page = $request->get('page');
+
+        $collection = Teacher::
+            select('teachers.*', 'users.firstname', 'users.lastname')
+            ->leftJoin('users', function($join){
+                $join->on('teachers.user_id', '=', 'users.id');
+            })
+            ->paginate(1, '*', 'page', 2);
+            //->get();
+        $records = [];
+        foreach($collection as $item){
+            $records[] = $item;
+        }
 
         $tpl = $this->getTpl('teachers/index');
 
