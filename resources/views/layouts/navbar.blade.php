034bd34884bae804ab6478386e263ccf55fba1fa
nekosama <kevinorge@gmail.com>
2019-09-30 02:59:01 +0800

    change home url

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,7 +2,7 @@
 	<nav class="uk-navbar-container" uk-navbar>
 		<div class="uk-navbar-left">
 					<ul class="uk-navbar-nav">
-							<li class="uk-active"><a href="">Home</a></li>
+							<li class="uk-active"><a href="/">Home</a></li>
 							<li><a href="/admin/teachers">Teachers</a></a></li>
 							<li><a href="/admin/students">Students</a></li>
 							<li><a href="/admin/classrooms">Classrooms</a></li>
