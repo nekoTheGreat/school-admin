19e811d9fdd824ceebdb1b814fe8d70e10479c49
nekosama <kevinorge@gmail.com>
2019-09-30 03:16:04 +0800

    added lib icons

--- resources/views/layouts/bottom.blade.php
+++ resources/views/layouts/bottom.blade.php
@@ -1,4 +1,5 @@
 	</body>
 	<script src="/libs/jquery/jquery-3.4.1.js"></script>
 	<script src="/libs/uikit-3.2.0/js/uikit.min.js"></script>
+	<script src="/libs/uikit-3.2.0/js/uikit-icons.min.js"></script>
 </html>
