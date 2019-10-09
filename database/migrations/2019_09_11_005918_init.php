5002bbfe8c34b9b20e9e0d3f15330cc49fe67089
nekosama <kevinorge@gmail.com>
2019-10-09 09:38:49 +0800

    modified main db blueprint using the current migration files then deleted current migration files

--- database/migrations/2019_09_11_005918_init.php
+++ database/migrations/2019_09_11_005918_init.php
@@ -65,6 +65,7 @@ class Init extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('category');
+            $table->string('grade_level')->nullable(true);
             $table->dateTime('created_at');
             $table->dateTime('updated_at')->nullable(true);
             $table->string('created_by');
@@ -130,6 +131,14 @@ class Init extends Migration
             $table->foreign('subject_id')->references('id')->on('subjects');
             $table->foreign('student_id')->references('id')->on('students');
         });
+
+        Schema::create('education_stages', function(Blueprint $table){
+			$table->bigIncrements('id');
+            $table->string('stage');
+            $table->string('level');
+            $table->string('created_at');
+            $table->string('created_by');
+		});
     }
 
     /**
@@ -139,15 +148,16 @@ class Init extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('roles');
-        Schema::dropIfExists('classrooms');
-        Schema::dropIfExists('students');
-        Schema::dropIfExists('teachers');
-        Schema::dropIfExists('subjects');
         Schema::dropIfExists('classroom_teachers');
         Schema::dropIfExists('teacher_students');
         Schema::dropIfExists('classroom_students');
         Schema::dropIfExists('markings');
+        Schema::dropIfExists('classrooms');
+        Schema::dropIfExists('students');
+        Schema::dropIfExists('teachers');
+        Schema::dropIfExists('subjects');
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('education_stages');
     }
 }
