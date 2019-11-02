c2f1516b6ed79a8f41194afdf3a1f1413e94aa5b
nekosama <kevinorge@gmail.com>
2019-11-02 14:54:35 +0800

    remove unused route

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+*/
 Route::group(['prefix'=> 'subjects'], function(){
 
     Route::group(['prefix'=> 'teachers/{teacher_id}'], function(){
