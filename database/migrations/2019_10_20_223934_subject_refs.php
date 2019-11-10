d4b91c03c69ec79fe7e7368b3eb9470beec5b6fe
nekosama <kevinorge@gmail.com>
2019-11-11 04:45:33 +0800

    modified migration to subjects related

--- database/migrations/2019_10_20_223934_subject_refs.php
+++ database/migrations/2019_10_20_223934_subject_refs.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class SubjectTeacher extends Migration
+class SubjectRefs extends Migration
 {
     /**
      * Run the migrations.
@@ -22,6 +22,15 @@ class SubjectTeacher extends Migration
             $table->foreign('teacher_id')->references('id')->on('teachers');
             $table->foreign('subject_id')->references('id')->on('subjects');
         });
+
+        Schema::create('student_subjects', function(Blueprint $table){
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('subject_id');
+
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->foreign('subject_id')->references('id')->on('subjects');
+        });
     }
 
     /**
@@ -33,5 +42,6 @@ class SubjectTeacher extends Migration
     {
         //
         Schema::drop('teacher_subjects');
+        Schema::drop('student_subjects');
     }
 }
